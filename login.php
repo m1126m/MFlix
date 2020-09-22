f1bb7e5b2aa7d3b97d5b00c11d64405d5b3ecc68
mm11-26 <mitsuhiro.makino777@gmail.com>
2020-09-22 10:07:01 +0200

    create login page

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+    if(isset($_POST["submitButton"])) {
+        echo "Form was submitted";
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Netflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+    </head>
+    <body>
+       <div class="signInContainer">
+       
+        <div class="column">
+       
+            <div class="header">
+                <img src="assets/images/logo.png" title="Logo" alt="Site logo"/>
+                <h3>Sign In</h3>
+                <span>to continue to Mflix</span>
+            </div>
+
+            <form method="POST">
+
+                <input type="text" name="userName" placeholder="User name" required>
+
+                <input type="password" name="password" placeholder="Password" required>
+                
+                <input type="submit" name="submitButton" value="SUBMIT">
+
+            </form>
+
+            <a href="register.php" class="signInMessage">Need an account? Sign up here</a>
+
+        </div>
+       </div>
+    </body>
+</html>
